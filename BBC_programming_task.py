63bb28ff042bc44bc5094336d9cbaf97d9fdeebb
Phoebe Wheeler <phoebe.wheeler0503@gmail.com>
2018-12-04 19:31:36 +0000

    Adding URL validation

--- BBC_programming_task.py
+++ BBC_programming_task.py
@@ -1,4 +1,5 @@
 import urllib.request
+import re
 
 webAddresses = []
 
@@ -14,5 +15,20 @@ while True: #loop until break
     else: #no newlines, no break
         webAddresses.append(inputText) #append input to list
 
+validURL = re.compile(
+    r'^(?:http|ftp)s?://' # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+    r'localhost|' #localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+    r'(?::\d+)?' # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+def checkURL(url):
+    return re.match(validURL, url)
+
 for address in webAddresses: #loop through contents of list
-    print(address) # temp - for now, print element of list
+    #print(address) # temp - for now, print element of list
+    if checkURL(address):
+        print("Acceptable URL: " + address + "\n")
+    else:
+        print("Unacceptable URL: " + address + "\n")
